3c72501b424e38206df66547846b8206a1c5c44f
Paul Kamma <paul.kamma@googlemail.com>
2018-12-03 10:38:31 +0100

    Update src templates

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -1,21 +1,3 @@
-(function(){
-
-  function sayHello2(){
-    console.log('breadcrtumb');
-    sayHello3();
-  }
-
-  function sayHello3(){
-    console.log('Hello');
-    sayHello();
-  }
-
-
-  function sayHello(){
-    console.log('Hello');
-    sayHello2();
-    sayHello2();
-  }
-// Das ist ein test
-sayHello();
+(function() {
+	// Breadcrtumb test
 })();
--- src/js/template/sentinel.template.js
+++ src/js/template/sentinel.template.js
@@ -1,25 +1,29 @@
-(function(){
-  var modules = ['###MODULES###'];
+(function() {
+	var modules = ['###MODULES###'];
+	var jsPath = './js/';
+	var modulePrefix = 'xt';
 
-  function loadScript(name){
-    var js_script = document.createElement('script');
-    js_script.type = "text/javascript";
-    js_script.src = name + '.js';
-    js_script.async = true;
-    document.getElementsByTagName('head')[0].appendChild(js_script);
-  }
+	function loadScript(name) {
+		var js_script = document.createElement('script');
+		js_script.type = 'text/javascript';
+		js_script.src = jsPath + name + '.js';
+		js_script.async = true;
+		document.getElementsByTagName('head')[0].appendChild(js_script);
+	}
 
-  document.addEventListener("DOMContentLoaded", function(event) {
-    var modulesOnPage = modules.map( function(moduleName) {
-      var dom = document.querySelector('[data-vhv-module-' + moduleName + ']');
-      return dom != null ? null : moduleName;
-    })
-    .filter( function(moduleName) { return modleName != null });
+	document.addEventListener('DOMContentLoaded', function(event) {
+		var prefix = modulePrefix !== '' ? modulePrefix + '-' : '';
 
-    modulesOnPage.forEach( function(filename){
-      loadScript(filename);
-    });
-
-  });
-
+		var modulesOnPage = modules
+			.map(function(moduleName) {
+				var dom = document.querySelector('[data-' + prefix + 'module-' + moduleName + ']');
+				return dom == null ? null : moduleName;
+			})
+			.filter(function(moduleName) {
+				return moduleName != null;
+			});
+		modulesOnPage.forEach(function(filename) {
+			loadScript(filename);
+		});
+	});
 })();
